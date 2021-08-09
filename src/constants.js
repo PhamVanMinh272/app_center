0e17e7422f93582c3b4f791d9e1ea1b3bbcd5fd0
phamvanminh272 <phamvanminh272@gmail.com>
2021-08-09 22:34:17 +0700

    add task resoler task

--- src/constants.js
+++ src/constants.js
@@ -8,5 +8,10 @@ export const APP_LIST = [
         name: "Vocab",
         link: "https://vocab-ielts.herokuapp.com",
         description: "This application will help you save and get testing your vocabulary."
+    },
+    {
+        name: "Task_resolver",
+        link: "http://PhamVanMinh272.github.io/task_resolver",
+        description: "This application will help you manage your tasks and provides some tools to analize your problems."
     }
 ]
